11682b908c84471be50df1856835811193d8ebfc
Nicolas HICHER (atarakt) <nhicher@proformatique.com>
2012-10-09 13:57:50 -0400

    [evo] fix package name in setup.py

--- xivo-fetchfw/setup.py
+++ xivo-fetchfw/setup.py
@@ -5,17 +5,17 @@
 from distutils.core import setup
 
 setup(
-    name='fetchfw',
+    name='xivo-fetchfw',
     version='1.0',
     description='Library and tool for downloading and installing remote files.',
     maintainer='Proformatique',
     maintainer_email='technique@proformatique.com',
     url='http://wiki.xivo.fr/',
     license='GPLv3',
-    packages=['fetchfw'],
+    packages=['xivo-fetchfw'],
     scripts=['bin/xivo-fetchfw'],
-    data_files=[('/etc/pf-xivo', ['resources/etc/fetchfw.conf']),
-                ('/var/lib/pf-xivo-fetchfw/installable',
+    data_files=[('/etc/pf-xivo', ['resources/etc/xivo-fetchfw.conf']),
+                ('/var/lib/xivo-fetchfw/installable',
                       ['resources/data/files.db',
                        'resources/data/install.db',
                        'resources/data/packages.db'])]
