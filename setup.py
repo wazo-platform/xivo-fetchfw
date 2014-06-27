3016ff859979172c2d612add0cea1ace04c9fd54
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2014-06-27 15:16:26 -0400

    sed -i 's/xivo.fr/xivo.io/g'

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='Library and tool for downloading and installing remote files.',
     maintainer='Proformatique',
     maintainer_email='technique@proformatique.com',
-    url='http://wiki.xivo.fr/',
+    url='http://wiki.xivo.io/',
     license='GPLv3',
     packages=['xivo_fetchfw',
               'xivo_fetchfw.cisco'],
