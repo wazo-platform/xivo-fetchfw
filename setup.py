ccc159c3dbfc68e95affcd730d1641c2ec5c7986
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-26 09:30:44 -0400

    update wazo email

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
 from setuptools import find_packages
@@ -9,7 +9,7 @@ setup(
     version='1.0',
     description='Library and tool for downloading and installing remote files.',
     author='Wazo Authors',
-    author_email='dev.wazo@gmail.com',
+    author_email='dev@wazo.community',
     url='http://wazo.community',
     license='GPLv3',
     packages=find_packages(),
